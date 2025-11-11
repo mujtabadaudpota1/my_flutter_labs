178beb9f4116a3f52ffc207d8c8bf9922d0f32d4
Ghulam Mujtaba <mujt0006@algonquinlive.com>
2025-11-11 14:22:30 -0500

    Week9 done

--- lib/app_database.dart
+++ lib/app_database.dart
@@ -9,5 +9,5 @@ part 'app_database.g.dart';
 
 @Database(version: 1, entities: [ToDoItem]) // 👈 entity included
 abstract class AppDatabase extends FloorDatabase {
-  ToDoDao get todoDao; // 👈 matches your DAO class name
+  ToDoDao get todoDao; //
 }
